6673255218bf9a2a6c89d1afb78d684c1d6eb141
Seaman <34seaman34@gmail.com>
2025-04-13 21:34:42 +0300

    paint ot black 0.3v

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var zIndex = 1;
-
+var isPaintMode = false;
 class cells {
     cell;
     constructor(cell) {
         this.cell = cell;
     }
-
     grab() {
         this.MouseDown = false;
         this.offsetX = 0;
@@ -38,7 +37,6 @@ class cells {
         });
     }
 }
-
 class buttons{
     button;
     method;
@@ -46,30 +44,74 @@ class buttons{
         this.button = button;
         this.method = method;
     }
-    createCell() {
-        var newDiv = document.createElement("div");
-        var newSpan = document.createElement("span");
-        newSpan.innerHTML = "new notice"
-        newDiv.append(newSpan);
-        newDiv.classList.add("cell");
-        document.querySelector(".freeArea").appendChild(newDiv);
-        arrayCell.push(new cells(newDiv));
-        arrayCell[arrayCell.length - 1].grab();
-
+    createCell(quantity = 1) {
+        for(var i=0;i<quantity ;i++){
+            var newDiv = document.createElement("div");
+            var newSpan = document.createElement("span");
+            newSpan.contentEditable = "true";
+            newSpan.innerHTML = "new notice";
+            newDiv.append(newSpan);
+            newDiv.classList.add("cell");
+            document.querySelector(".freeArea").appendChild(newDiv);
+            arrayCell.push(new cells(newDiv));
+            arrayCell[arrayCell.length - 1].grab();
+        }
+    }
+    activePaint(){
+        isPaintMode ? isPaintMode=false : isPaintMode=true;
+        pointerEvent(isPaintMode);
+    }
+}
+//paint =>
+var canvas = document.querySelector("canvas");
+canvas.classList.add("none-events");
+var ctx = canvas.getContext("2d");
+var isDrawing = false;
+var x;
+var y;
+function pointerEvent(none){
+    if(none){
+        canvas.classList.remove("none-events");
+        arrayCell.forEach((element,i) => {
+            document.querySelectorAll(".cell")[i].classList.add("none-events");
+        });
+    }else{
+        canvas.classList.add("none-events");
+        arrayCell.forEach((element,i) => {
+            document.querySelectorAll(".cell")[i].classList.remove("none-events");
+        });
     }
 }
 
+function paint(e){
+    if (!this.isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    x = e.offsetX;
+    y = e.offsetY;
+}
+//paint <=
+var arrayCopies=[
+    copyButton = new buttons(undefined,undefined),
+];
 var arrayCell = [
-    new cells(document.querySelectorAll(".cell")[0]),
+    new cells(document.querySelector(".cell"))
 ];
-
 var arrayButtons = [
-    copy = new buttons(undefined,undefined),
-    createButton = new buttons(document.getElementById("createCell"),copy.createCell),
+    createButton = new buttons(document.getElementById("createCell"),copyButton.createCell),
+    paintButton = new buttons(document.getElementById("paintButton"),copyButton.activePaint)
 ];
-//включаем кнопки
+function getRandomColor() {
+    const someColors = ["#d1c30a","#990850","#238510","#0a13c7","#ba4907","#990e84","#077d8f"];
+    var color ;
+    color = someColors[Math.floor(Math.random() * someColors.length)];
+    return color;
+}
+//включаем 
 function turnOn(){
-    arrayCell[0].grab();
+    //кнопки
     arrayButtons.forEach(element => {
         try{
             element.button.addEventListener("click", () => {
@@ -79,6 +121,23 @@ function turnOn(){
             console.error("undefined passed")
         }
     });
+    //красим "paint"
+    for(var i =0;i<6;i++){
+        document.querySelectorAll(".rainbowLetter")[i].style.color = getRandomColor();
+    }
+    //заметка
+    arrayCell[0].grab();
+    //холст
+    canvas.addEventListener("mousedown", (e) =>{
+        isDrawing = true;
+        x = e.offsetX;
+        y = e.offsetY;
+    });
+    canvas.addEventListener("mouseout", (e)=>{
+        paint(e);
+        isDrawing = false;
+    });
+    canvas.addEventListener("mousemove", (e)=>{paint(e);});
+    canvas.addEventListener("mouseup", (e)=>{isDrawing = false;});
 }
-
 turnOn();
